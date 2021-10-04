4f53dd6ae8afdde5d154e3e26370b5a63be806b2
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-10-03 22:30:33 -0500

    Agregando componente Animacion1

---
+++ components/Animacion1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, Text, Animated} from 'react-native';
+
+const Animacion1 = () => {
+  return (
+    <>
+      <Text>Animacion 1</Text>
+    </>
+  );
+};
+
+export default Animacion1;
