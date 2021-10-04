c900bd69c7fe505ed558a748bf214bf1e3818aab
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-10-03 22:37:59 -0500

    Animando la opacidad de un texto al iniciar la app

--- components/Animacion1.js
+++ components/Animacion1.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import {StyleSheet, Text, Animated} from 'react-native';
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable prettier/prettier */
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, Text, Animated, NativeModules} from 'react-native';
 
 const Animacion1 = () => {
+  const [animacion] = useState(new Animated.Value(0));
+  useEffect(() => {
+    Animated.timing(animacion, {
+      toValue: 1, // al valor final
+      duration: 500, // duracion
+    }).start(); // inicia la animacion
+  }, []);
   return (
     <>
-      <Text>Animacion 1</Text>
+      <Animated.View
+        style={{
+            opacity: animacion,
+        }}
+      >
+        <Text style={styles.texto}>Animacion 1</Text>
+      </Animated.View>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  texto: {
+    fontSize: 30,
+    textAlign: 'center',
+  },
+});
 
 export default Animacion1;
